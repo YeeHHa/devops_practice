b10568e855c37a095f29e806cf83acf023e382f7
YeeHHa <matthew.cradit@gmail.com>
2022-01-29 18:44:23 -0600

    moved Jenkinsfile to jenkins dir

--- main_engine.py
+++ main_engine.py
@@ -1,4 +1,13 @@
 #initial python flask project
+from flask import Flask
 
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'hello world'
+
+if __name__ == '__main__':
+    app.run()
 
 
